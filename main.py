c8221c7cf65edf792510b5204a4057e2cbc74bbd
steve ‎ <176925833+mr-steveryan@users.noreply.github.com>
2026-07-16 23:11:28 +0530

    stage 4: full CRUD

--- main.py
+++ main.py
@@ -37,9 +37,43 @@ def add_task(record: dict):
     if 'title' not in record or not isinstance(record['title'],str):
         return JSONResponse(
             status_code=400,
-            content={'error':'title should be a string'}
+            content={'error':'title should exist and should be a string'}
         )
     task_id = mem[-1]['id'] + 1
     task = {"id":task_id, "title":record['title'], "Done":False}
     mem.append(task)
     return {"message": "created", "task": task}
+
+@app.put('/tasks/{task_id}')
+def update_task(task_id: int, record: dict):
+    if 'title' not in record or not isinstance(record['title'],str):
+        return JSONResponse(
+            status_code=400,
+            content={'error':'title should exist and should be a string'}
+        )
+    if 'done' not in record or not isinstance(record['done'],bool):
+        return JSONResponse(
+            status_code=400,
+            content={'error':'done should exist and should be a boolean'}
+        )
+    for item in mem:
+        if item['id'] == task_id:
+            item['title']=record['title']
+            item['done']=record['done']
+            return item
+    return JSONResponse(
+        status_code=404,
+        content={'error':f"record with id: {task_id} doesn't exist"}
+    )
+    
+@app.delete('/tasks/{task_id}', status_code=204)
+def delete_task(task_id: int):
+    for item in mem:
+        if item['id'] == task_id:
+            mem.remove(item)
+            return
+    return JSONResponse(
+        status_code=404,
+        content={'error':f"record with id: {task_id} doesn't exist"}
+    )
+    
